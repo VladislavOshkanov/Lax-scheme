8aabdbdba88a6c21df71edb9354f1c4eece3b388
Vlad_Oshkanov <ovs@live.ru>
2015-11-27 22:58:49 +0600

    It works. I think

--- Lax.cpp
+++ Lax.cpp
@@ -3,7 +3,6 @@
 #include<stdlib.h>
 #include<iostream>
 using namespace std;
-const double A = 0.034;
 
 double m2 (double t){
 	return (1.21) - sin(2 * M_PI * t) / 2 + 1 - 3.5 * t;
@@ -14,14 +13,11 @@ double m1 (double t){
 double m (double x){
 	return pow((x+0.1),2) + x;
 }
-double courant_number(double A, double tao, double h){
-	return 2*A*tao/pow(h,2);
-}
 double C(double t, double x){
 	return (M_PI*cos(2*M_PI*t) + 3.5)/(2*x+1.2);
 }
 double u(double x, double t){
- return (x + 0.1)*(x + 0.1) - sin (2 * M_PI * t) / 2 + x - 3.5 * t;
+ return pow((x + 0.1), 2) - sin (2 * M_PI * t) / 2 + x - 3.5 * t;
 }
 
 int main (){
@@ -30,7 +26,9 @@ int main (){
 	func.open("func.txt");
 	cout.precision(4);
    	double *x, *t, tao, h;
-	cin >> tao >> h;
+	cout << "Please, insert Tao and H. Don't forget about Courant condition" << endl;
+	cin >> tao;
+	cin >> h;
 	int N = fmax (fabs(1/tao)+10, fabs(1/h)+10);//crutch
 	int i = 0;
 	x = (double*)calloc(sizeof(double*), N);
@@ -69,24 +67,25 @@ int main (){
 
 
 	for (int k = 1; k < tao_length; k++){
-		for (i = 0; i < x_length-2; i++){
+		for (i = 1; i < x_length - 1; i++){
+		 U[k][i] = (U[k-1][i+1]+U[k-1][i-1])/2 - tao*C(t[k - 1],x[i])*(U[k-1][i+1]-U[k-1][i-1])/(2*h); 	
 		}
 		
 	}
-	int sx = x_length / 30;
-	int stao  = tao_length / 30;	
+	//int sx = x_length / 30;
+	//int stao  = tao_length / 30;	
+	int sx = 1;
+	int stao = 1;
 	for (i = 0; i < tao_length; i++){
 		for (int j = 0; j < x_length; j++){
-				if (i % sx == 0 && j % stao == 0){
-				approx << x[i] << " " << t[j] << " ";
-				approx << U[i][j] << endl;
-				func << x[i] << " " << t[j] << " ";
-				func << u(x[j], t[i]) << endl;
-				}
+				approx << x[j] << " " << t[i] << " " << U[i][j] << endl;
+				func << x[j] << " " << t[i] << " " << u(x[j],t[i]) << endl;
 			}
+			func << endl; approx<< endl;
 	}
 	double max = 0;
-	for (i = 0; i < tao_length; i++)
+	cout << endl;
+	for (i = 0; i < tao_length -2; i++)
 		for (int j = 0; j < x_length; j++)
 		   	if (fabs(U[i][j]-u(x[j], t[i])) > max)  max = fabs(U[i][j]-u(x[j],t[i]));
 	cout << max << endl;
